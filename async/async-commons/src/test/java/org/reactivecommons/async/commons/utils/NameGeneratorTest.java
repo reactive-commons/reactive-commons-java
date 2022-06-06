61a679f333b2aee0ddd16084d7ab015172104ee4
kvaithin <karthik.vaithin@aexp.com>
2022-06-06 22:33:37 +0100

    73: Updated tests for NameGenerator

---
+++ async/async-commons/src/test/java/org/reactivecommons/async/commons/utils/NameGeneratorTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NameGeneratorTest {
+
+    private NameGenerator nameGenerator;
+
+    @Test
+    void generateNameFromWithoutSuffix() {
+        String result = NameGenerator.generateNameFrom("application");
+        assertFalse(result.contains("="));
+        assertTrue(result.startsWith("application--"));
+        assertEquals(35, result.length());
+    }
+
+    @Test
+    void generateNameFromWithSuffix() {
+        String result = NameGenerator.generateNameFrom("application", "suffix");
+        assertFalse(result.contains("="));
+        assertTrue(result.startsWith("application-suffix-"));
+        assertEquals(41, result.length());
+    }
+}
